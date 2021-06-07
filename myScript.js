58f3b62db1ab04b9e3b549bf5e7afbe5df885ac0
Wilson Lin <wilsonlin9506@gmail.com>
2021-06-07 17:47:26 -0400

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -1,24 +1,24 @@
-<!DOCTYPE html>
-<html>
-  <head>
-// <!--     <title>Simple Map</title>
-//     <meta name="viewport" content="initial-scale=1.0">
-//     <meta charset="utf-8">
-//     <style>
-//       /* Always set the map height explicitly to define the size of the div
-//        * element that contains the map. */
-//       #map {
-//         height: 100%;
-//       }
-//       /* Optional: Makes the sample page fill the window. */
-//       html, body {
-//         height: 100%;
-//         margin: 0;
-//         padding: 0;
-//       }
-//     </style> -->
-  </head>
-  <body>
+// <!DOCTYPE html>
+// <html>
+//   <head>
+// // <!--     <title>Simple Map</title>
+// //     <meta name="viewport" content="initial-scale=1.0">
+// //     <meta charset="utf-8">
+// //     <style>
+// //       /* Always set the map height explicitly to define the size of the div
+// //        * element that contains the map. */
+// //       #map {
+// //         height: 100%;
+// //       }
+// //       /* Optional: Makes the sample page fill the window. */
+// //       html, body {
+// //         height: 100%;
+// //         margin: 0;
+// //         padding: 0;
+// //       }
+// //     </style> -->
+//   </head>
+//   <body>
 // <!--     <div id="map"></div>
 //     <script>
 //       var map;
@@ -34,8 +34,8 @@
       function myFunction() {
         document.getElementById("demo").innerHTML = "Let's do it!";
       }
-  </body>
-</html>
+//   </body>
+// </html>
 
 // <style>
 //   #map {
