8c47e0fc457deb783c5bcf58e27224e350ca6780
Wilson Lin <wilsonlin9506@gmail.com>
2021-06-07 17:48:51 -0400

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -1,49 +1,49 @@
-// <!DOCTYPE html>
-// <html>
-//   <head>
-// // <!--     <title>Simple Map</title>
-// //     <meta name="viewport" content="initial-scale=1.0">
-// //     <meta charset="utf-8">
-// //     <style>
-// //       /* Always set the map height explicitly to define the size of the div
-// //        * element that contains the map. */
-// //       #map {
-// //         height: 100%;
-// //       }
-// //       /* Optional: Makes the sample page fill the window. */
-// //       html, body {
-// //         height: 100%;
-// //         margin: 0;
-// //         padding: 0;
-// //       }
-// //     </style> -->
-//   </head>
-//   <body>
-// <!--     <div id="map"></div>
-//     <script>
-//       var map;
-//       function initMap() {
-//         map = new google.maps.Map(document.getElementById('map'), {
-//           center: {lat: -34.397, lng: 150.644},
-//           zoom: 8
-//         });
-//       }
-//     </script>
-//     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBEwTHbtuO_TmNdoxPvyyrNCMkzDzeBfp0&callback=initMap"
-//     async defer></script> -->
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Simple Map</title>
+    <meta name="viewport" content="initial-scale=1.0">
+    <meta charset="utf-8">
+    <style>
+      /* Always set the map height explicitly to define the size of the div
+       * element that contains the map. */
+      #map {
+        height: 100%;
+      }
+      /* Optional: Makes the sample page fill the window. */
+      html, body {
+        height: 100%;
+        margin: 0;
+        padding: 0;
+      }
+    </style>
+  </head>
+  <body>
+    <div id="map"></div>
+    <script>
+      var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 8
+        });
+      }
+    </script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBEwTHbtuO_TmNdoxPvyyrNCMkzDzeBfp0&callback=initMap"
+    async defer></script>
       function myFunction() {
         document.getElementById("demo").innerHTML = "Let's do it!";
       }
-//   </body>
-// </html>
+  </body>
+</html>
 
-// <style>
-//   #map {
-//     height: 100%;
-//   }
-//   html, body {
-//     height: 100%;
-//     margin: 0;
-//     padding: 0;
-//   }
-// </style>
+<style>
+  #map {
+    height: 100%;
+  }
+  html, body {
+    height: 100%;
+    margin: 0;
+    padding: 0;
+  }
+</style>
